3898b72f94d4eb11f74e46799755f4bb31010ada
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-01-05 17:53:30 +0800

    remove kmm files

--- 02 - Getting Started/escape_characters.rb
+++ 02 - Getting Started/escape_characters.rb
@@ -6,3 +6,13 @@ puts 'Juliet said \'Goodbye\' to Romeo'
 
 puts "Juliet said 'Goodbye' to Romeo"
 puts 'Juliet said "Goodbye" to Romeo'
+
+#kmm
+puts "some text\nmore text"
+puts "\tOnce upon a time"
+
+puts "Juliet said \"Goodbye\" to Romeo"
+puts 'Juliet said \'Goodbye\' to Romeo'
+
+puts "Juliet said 'Goodbye' to Romeo"
+puts 'Juliet said "Goodbye" to Romeo'
--- 02 - Getting Started/kmm-escape_characters.rb
+++
@@ -1,8 +0,0 @@
-puts "some text\nmore text"
-puts "\tOnce upon a time"
-
-puts "Juliet said \"Goodbye\" to Romeo"
-puts 'Juliet said \'Goodbye\' to Romeo'
-
-puts "Juliet said 'Goodbye' to Romeo"
-puts 'Juliet said "Goodbye" to Romeo'
