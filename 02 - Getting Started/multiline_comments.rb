547343772770ec22d79d90984d63e5b572a2cbc9
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-01-05 19:10:35 +0800

    26. Multiline Comments - 2min

---
+++ 02 - Getting Started/multiline_comments.rb
@@ -0,0 +1,19 @@
+=begin
+Hello there!
+This is a multi-line comment
+Lots of fun code down below
+Have a blast!
+=end
+
+puts 1 + 3
+
+#kmm
+
+=begin
+Hello there!
+This is a multi-line comment
+Lots of fun code down below
+Have a blast!
+=end
+
+puts 1 + 3
