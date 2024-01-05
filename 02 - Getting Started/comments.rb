01598332f13b68e66ee4117e082e59f0c85f4469
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-01-05 19:05:04 +0800

    25. Comments - 7min

---
+++ 02 - Getting Started/comments.rb
@@ -0,0 +1,16 @@
+# The code below adds 2 numbers together
+# Another comment right here
+# Another comment down below
+puts 1 + 1
+puts 2 + 2 # This will still work
+puts 3 + 3
+
+#kmm
+
+# The code below adds 2 numbers together
+# Another comment right here
+# Another comment down below
+puts 1 + 1
+puts 2 + 2 # This will still work
+puts 3 + 3
+
