456d689ffe27964691048d7a27539156947fecb0
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-04-26 14:42:20 +0800

    85. if and elsif - 11min

---
+++ 08 - Conditionals/if_and_elsif.rb
@@ -0,0 +1,27 @@
+color = "Red"
+
+if color == "Red"
+  puts "Red is rad"
+elsif color == "Yellow"
+  puts "Yay for yellow"
+elsif color == "Green"
+  puts "Green is great"
+end
+
+number = 10
+
+if number < 25
+  puts "That's a small number"
+elsif number < 50
+  puts "That's a medium-sized number"
+elsif number < 75
+  puts "That's a big number"
+elsif number < 150
+  puts "That's a humongous better"
+end
+
+if number < 25
+  puts "First condition is true"
+elsif color == "Green"
+  puts "Second condition is true"
+end
