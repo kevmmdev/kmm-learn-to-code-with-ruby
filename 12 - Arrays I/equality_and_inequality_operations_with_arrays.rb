fa5842defdbf224536c97e30261ccfb6d72c5f14
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-04-27 17:54:16 +0800

    144. Equality and Inequality Operators with Arrays - 4min

---
+++ 12 - Arrays I/equality_and_inequality_operations_with_arrays.rb
@@ -0,0 +1,14 @@
+ candy = ["Skittles", "Starbursts", "Snickers"]
+ vegetables = ["Radishes", "Onions"]
+ desserts = ["Skittles", "Starbursts", "Snickers"]
+ sweets = ["Skittles", "Starbursts", "snickers"]
+
+ puts candy == desserts
+ puts candy == vegetables
+ puts candy == sweets
+
+ puts
+
+ puts candy != desserts
+ puts candy != vegetables
+ puts candy != sweets
