049ef4eea652249525dc627fe192a7e057163c92
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-04-27 16:18:19 +0800

    135. Access and Overwrite Array Element by Index Position - 9min

---
+++ 12 - Arrays I/access_and_overwrite_array_element_by_index_position.rb
@@ -0,0 +1,34 @@
+fruits = ["Apple", "Orange", "Grape", "Banana"]
+
+puts fruits[0]
+puts fruits[1]
+puts fruits[3]
+p fruits[100]
+
+puts
+
+puts fruits[-1]
+puts fruits[-2]
+puts fruits[-4]
+p fruits[-10]
+
+puts
+
+puts fruits.slice(0)
+p fruits.slice(10)
+p fruits.slice(-3)
+
+puts
+
+puts fruits.[](0)
+
+puts
+
+fruits[1] = "Watermelon"
+p fruits
+
+fruits[4] = "Raspberry"
+p fruits
+
+fruits[10] = "Kiwi"
+p fruits
