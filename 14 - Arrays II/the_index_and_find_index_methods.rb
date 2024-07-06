d1eb5a4e4bca911e04e1eb97aa926499d48e08ec
Kevin Mark Montesclaros <kevmm.dev@gmail.com>
2024-07-06 22:56:58 +0800

    174. The index and find_index Methods - 2min

---
+++ 14 - Arrays II/the_index_and_find_index_methods.rb
@@ -0,0 +1,11 @@
+# index/find_index - return the index position of the first occurrence of element
+
+colors = ["Red", "Blue", "Green", "Red"]
+
+p colors.index("Green")
+p colors.index("Red")
+p colors.index("Orange")
+
+p colors.find_index("Green")
+p colors.find_index("Red")
+p colors.find_index("Orange")
