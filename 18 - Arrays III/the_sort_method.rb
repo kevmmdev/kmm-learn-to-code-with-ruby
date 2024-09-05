2942c46762d5782a0b98aae8f7295bc0f02c5605
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-09-06 03:45:05 +0800

    216. The sort Method - 3min

---
+++ 18 - Arrays III/the_sort_method.rb
@@ -0,0 +1,10 @@
+# sort method - sorts an array in ascending/alphabetical order
+
+p [2, 3, 1].sort
+p [2, 3, 1].sort.reverse
+
+p ["Hello", "Blah", "Zebra"].sort
+p ["Hello", "Blah", "Zebra"].sort.reverse
+p ["Hello", "zebra", "Blah", "Zebra", "artist"].sort
+
+# A-Z comes before a-z
