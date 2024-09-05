cee4bd1d8eb8e75455071f86a62507a500bf2183
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-09-06 03:40:33 +0800

    215. The reverse Method - 2min

---
+++ 18 - Arrays III/the_reverse_method.rb
@@ -0,0 +1,8 @@
+# reverse - returns a new array with elements in reverse order
+
+p [1, 2, 3].reverse
+p [true, true, false, false].reverse
+
+queue = [4, 8, 15, 16, 23, 42]
+queue.reverse!
+p queue
