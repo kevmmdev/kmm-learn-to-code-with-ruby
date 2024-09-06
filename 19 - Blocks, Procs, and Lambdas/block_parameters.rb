978a5cca4f02306eeb3fc2e1c146bca70f2f1c6d
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-09-06 23:18:07 +0800

    231. Block Parameters - 10min

---
+++ 19 - Blocks, Procs, and Lambdas/block_parameters.rb
@@ -0,0 +1,14 @@
+def speak_the_truth(name)
+  yield(name)
+end
+
+
+speak_the_truth("Boris") { |name| puts "#{name} is brilliant!" }
+speak_the_truth("Sarah") { |name| puts "#{name} is incredible!" }
+
+def number_evaluation(num1, num2, num3)
+  yield(num1, num2, num3)
+end
+
+p number_evaluation(5, 10, 15) { |a, b, c| a + b + c }
+p number_evaluation(3, 4, 5) { |a, b, c| a * b * c }
