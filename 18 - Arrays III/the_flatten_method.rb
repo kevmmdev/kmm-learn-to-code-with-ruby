24fb08a75afe46302ed60755eafdab84ae592811
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-09-06 20:45:36 +0800

    220. The flatten Method - 3min

---
+++ 18 - Arrays III/the_flatten_method.rb
@@ -0,0 +1,10 @@
+# flatten method - create a 1-dimensional array (no nested arrays)
+
+party_attendees = [
+  "Joe",
+  ["Bob", ["Keith"], "Cam"],
+  "Holly",
+  ["Rick", [["Molly"]]]
+]
+
+p party_attendees.flatten
