56d4dad1672f05e930657a78b8802de9651b1b15
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-09-06 23:09:24 +0800

    230. The block_given? Method - 4min

---
+++ 19 - Blocks, Procs, and Lambdas/the_block_given?_method.rb
@@ -0,0 +1,11 @@
+# block_given? method determines whether a block was provided to method invocation
+
+def pass_control_on_condition
+  puts "Inside the method"
+  yield if block_given?
+  puts "Back inside the method"
+end
+
+pass_control_on_condition { puts "Inside the block" }
+puts
+pass_control_on_condition
