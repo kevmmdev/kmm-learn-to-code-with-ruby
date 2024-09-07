7e74d44e21c0691fde3dadcdd65d569eca5de220
Kevin Mark Montesclaros <kevmm.dev@gmail.com>
2024-09-07 22:33:34 +0800

    257. The attr_accessor Method - 2min

---
+++ 20 - Classes I/the_attr_accessor_method.rb
@@ -0,0 +1,26 @@
+class Guitar
+  attr_reader :type, :wood, :strings
+  attr_accessor :price
+
+  def initialize
+    @type = "Acoustic"
+    @wood = "Alder"
+    @strings = 6
+    @price = 1500
+  end
+
+  def to_s
+    "An #{@type} #{@wood} guitar with #{@strings} strings."
+  end
+end
+
+guitar = Guitar.new
+p guitar.type
+p guitar.wood
+p guitar.strings
+p guitar.price
+guitar.price=(5000)
+p guitar.price
+
+guitar.price = 10000
+p guitar.price
