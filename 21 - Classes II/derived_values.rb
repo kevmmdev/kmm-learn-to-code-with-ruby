3be724f5e4311ecd7c6b6a6913f03936254ca7d4
Kevin Mark Montesclaros <kevinmarkmontesclaros@gmail.com>
2024-09-08 03:13:44 +0800

    265. Derived Values - 8min

---
+++ 21 - Classes II/derived_values.rb
@@ -0,0 +1,25 @@
+# Anti-pattern - a "pattern" to avoid when writing code
+
+# Derived value - calculated/computed from another piece of state
+
+class Rectangle
+  attr_accessor :height, :width
+
+  def initialize(height, width)
+    @height = height
+    @width = width
+  end
+
+  def area
+    height * width
+  end
+end
+
+r = Rectangle.new(3, 5)
+p r.area
+
+r.height = 10
+p r.area
+
+r.width = 8
+p r.area
