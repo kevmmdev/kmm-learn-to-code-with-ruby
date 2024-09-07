042eaef17c19cc4bced2383a08828d419c87cfda
Kevin Mark Montesclaros <kevmm.dev@gmail.com>
2024-09-07 20:21:06 +0800

    247. Coding Exercise Solution: Instance Methods - 3min

---
+++ 20 - Classes I/override_the_to_s_method.rb
@@ -0,0 +1,14 @@
+class Guitar
+  def initialize
+    @type = "Acoustic"
+    @wood = "Alder"
+    @strings = 6
+  end
+
+  def to_s
+    "An #{@type} #{@wood} guitar with #{@strings} strings."
+  end
+end
+
+guitar = Guitar.new
+puts guitar
