e8954552c85549f65c58657857af3ec7f31a468a
Kevin Mark Montesclaros <kevmm.dev@gmail.com>
2024-09-08 14:57:07 +0800

    271. Monkey Patching - 10min

---
+++ 21 - Classes II/building_a_class_over_time.rb
@@ -0,0 +1,21 @@
+class Book
+  attr_reader :title, :author, :pages
+
+  def initialize(title, author, pages)
+    @title = title
+    @author = author
+    @pages = pages
+  end
+end
+
+goosebumps = Book.new("Night of the Living Dummy", "R.L. Stine", 100)
+# goosebumps.read
+
+class Book
+  def read
+    1.step(pages, 10) { |page| puts "Reading page #{page}" }
+    puts "Done! #{title} was a great book!"
+  end
+end
+
+goosebumps.read
