b5bfec4a022a0a6d02ba53a1965c940c394277ac
Kevin Mark Montesclaros <kevmm.dev@gmail.com>
2024-12-07 18:39:32 +0800

    361. Use Faker Gem - 8min

---
+++ 32 - Gems/use_faker_gem.rb
@@ -0,0 +1,12 @@
+require "faker"
+
+# puts Faker.class
+# puts Faker::Name.class
+# puts Faker::Name.name
+# puts Faker::Name.first_name
+# puts Faker::Name.last_name
+
+# p Faker.constants.sort
+
+puts Faker::Music.instrument
+puts Faker::Sports::Football.player
